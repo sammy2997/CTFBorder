a9a458718f3e698f7668c62ca11b0c3e378901d3
sammy2997 <samueladama@ymail.com>
2021-08-28 13:39:00 -0500

    final

---
+++ Example/Pods/Target Support Files/CTFBorder/CTFBorder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CTFBorder : NSObject
+@end
+@implementation PodsDummy_CTFBorder
+@end
---
+++ Example/Pods/Target Support Files/Pods-CTFBorder_Example/Pods-CTFBorder_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CTFBorder_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CTFBorder_Example
+@end
